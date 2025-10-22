03f66ec9498f964c8245c8c80ca4fa9425a9cf95
shyestfern <zalesak_maxmilian@oauh.cz>
2025-10-22 08:59:30 +0200

    Přidán navbar

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">Vlajky</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="<?= base_url('') ?>">Články</a>
+                    <a class="nav-link" href="<?= base_url('index.php/druha-stranka') ?>">Barvy</a>
+                    <a class="nav-link" href="<?= base_url('index.php/treti-stranka') ?>">Neobdélníkové</a>
+                    <a class="nav-link" href="<?= base_url('index.php/ctvrta-stranka') ?>">Symbolismus</a>
+                </div>
+            </div>
+        </div>
+    </nav>
