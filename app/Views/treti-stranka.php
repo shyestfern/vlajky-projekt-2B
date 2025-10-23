b401a62e9fc6a265fd6cd8805fadff03463cc574
shyestfern <zalesak_maxmilian@oauh.cz>
2025-10-23 23:24:19 +0200

    Přidán obsah třetí stránky

--- app/Views/treti-stranka.php
+++ app/Views/treti-stranka.php
@@ -1,4 +1,36 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
+
+<h1 class="mt-5 text-center">Neobdélníkové vlajky</h1>
+
+<div class="table-responsive mt-4">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col" class="w-25">Země</th>
+                <th scope="col" class="w-50">Tvar</th>
+                <th scope="col" class="w-25">Poměr stran</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><span class="fi fi-np me-2"></span>Nepál</td>
+                <td>Složená ze dvou trojúhelníků.</td>
+                <td>Nepravidelný</td>
+            </tr>
+            <tr>
+                <td><span class="fi fi-ch me-2"></span>Švýcarsko</td>
+                <td>Čtverec</td>
+                <td>1:1</td>
+            </tr>
+            <tr>
+                <td><span class="fi fi-va me-2"></span>Vatikán</td>
+                <td>Čtverec</td>
+                <td>1:1</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 <?= $this->endSection(); ?>
