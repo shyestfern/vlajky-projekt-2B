4e6dd947d5f6f1feaa91a05af911f7c17815c538
shyestfern <zalesak_maxmilian@oauh.cz>
2025-10-24 11:23:31 +0200

    Přidání indikátorů

--- app/Views/layout/css.php
+++ app/Views/layout/css.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css') ?>">
 <link rel="stylesheet" href="<?= base_url('node_modules/flag-icons/css/flag-icons.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('styles.css') ?>">
